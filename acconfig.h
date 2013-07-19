0e2c079b45c783131bfa0938de403d8fae972707
Martin Iturbide <martiniturbide@gmail.com>
2013-07-19 12:24:38 -0500

    Original Source Code - FCrackZip 0.2.4

---
+++ acconfig.h
@@ -0,0 +1,13 @@
+
+/* Define if you can use gcc inline assembly.  */
+#undef USE_GCC_ASM
+
+/* Define this to the default x86 target cpu (5 pentium, 6 ppro).  */
+#undef COMPILE_CPU
+
+/* Define if you can use unix-style redirection.  */
+#undef USE_UNIX_REDIRECTION
+
+#undef PACKAGE
+#undef VERSION
+
